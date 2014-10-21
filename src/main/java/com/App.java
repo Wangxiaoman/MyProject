9a005782e3121253c7e69ee459d57555038756c4
wangxiaoman <wangxiaoman@meituan.com>
2014-10-21 17:35:47 +0800

    x

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,8 +4,8 @@ package com;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+	
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
