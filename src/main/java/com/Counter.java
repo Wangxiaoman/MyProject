f21970a7b52b699783b6225d28e080ff0a29161e
wangxiaoman <283468598@qq.com>
2015-05-25 21:08:15 +0800

    x

--- src/main/java/com/Counter.java
+++ src/main/java/com/Counter.java
@@ -1,5 +1,8 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Counter {
 	private volatile int a =0;
@@ -31,7 +34,20 @@ public class Counter {
 	}
 	
 	public static void main(String[] args) {
-		number(3);
+//		number(3);
+		
+		Runtime r = Runtime.getRuntime();  
+		r.gc();  
+		long startMem = r.freeMemory(); // 开始时的剩余内存  
+		System.out.println("startMem:"+startMem);
+		List ol = new ArrayList();
+		for(int i=0;i<1000000;i++){
+			Object o = new Object(); 
+			ol.add(o);
+		}
+		System.out.println("lastMem:"+r.freeMemory());
+		long orz = startMem - r.freeMemory(); // 剩余内存 现在 - 开始 = o 的大小 
+		System.out.println("orz:"+orz);
 	}
 
 }
--- src/main/java/com/netty/DiscardServerHandler.java
+++ src/main/java/com/netty/DiscardServerHandler.java
@@ -6,14 +6,12 @@ import io.netty.channel.ChannelHandlerContext;
 
 public class DiscardServerHandler extends ChannelHandlerAdapter { // (1)
 
-    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
     	System.out.println(msg);
         // Discard the received data silently.
         ((ByteBuf) msg).release(); // (3)
     }
 
-    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
         // Close the connection when an exception is raised.
         cause.printStackTrace();
