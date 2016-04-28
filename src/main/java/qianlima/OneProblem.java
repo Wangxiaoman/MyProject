67358c838a40d5d5b46da86f6c887871e76055fb
wangxiaoman <283468598@qq.com>
2016-04-28 15:00:02 +0800

    【新增】千里码习题

---
+++ src/main/java/qianlima/OneProblem.java
@@ -0,0 +1,26 @@
+package qianlima;
+/**
+ * 第2333个能被2或者被3整除的正整数
+ * @author xiaoman
+ *
+ */
+public class OneProblem {
+ 
+  public static int getTheNumber(int nth){
+    int beginNumber = 1;
+    do{
+      beginNumber++;
+      if(beginNumber % 2 == 0 || beginNumber % 3 == 0){
+        nth--;
+      }
+    }while(nth > 0);
+    return beginNumber;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(getTheNumber(1));
+    System.out.println(getTheNumber(5));
+    System.out.println(getTheNumber(10));
+    System.out.println(getTheNumber(2333));
+  }
+}
