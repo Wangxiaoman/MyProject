6d308bb74538b15251cc4108b301fc6594b7db88
wangxiaoman <283468598@qq.com>
2016-06-22 12:00:44 +0800

    【新增】千里码openssl

---
+++ src/main/java/qlm/QLMModulo.java
@@ -0,0 +1,26 @@
+package qlm;
+
+public class QLMModulo {
+	
+	
+
+	public static void main(String[] args) {
+		int high = 3;
+		int width = 3;
+		int[][] array = new int[high][width];
+		array[0] = new int[]{1,0,0};
+		array[1] = new int[]{0,1,0};
+		array[2] = new int[]{0,1,1};
+		
+		int[] input1 = new int[]{1,1,1};
+		int[] input2 = new int[]{1};
+		int[][] input3 = new int[2][2];
+		input3[0] = new int[]{0,1};
+		input3[1] = new int[]{1,1};
+		int[][] input4 = new int[3][3];
+		input4[0] = new int[]{1};
+		input4[1] = new int[]{1};
+		input4[2] = new int[]{1};
+		
+	}
+}
