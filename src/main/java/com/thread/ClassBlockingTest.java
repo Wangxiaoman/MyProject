7cbb10a65d33e39aaf731bdb2cfeb01a58efde4a
王晓满 <simmer.wang@uxin.com>
2017-03-02 10:18:46 +0800

    【修改】

--- src/main/java/com/thread/ClassBlockingTest.java
+++ src/main/java/com/thread/ClassBlockingTest.java
@@ -12,14 +12,66 @@ public class ClassBlockingTest {
         System.out.println("b--123");
     }
     
-    public synchronized void c(){
+    public synchronized void c() throws Exception{
+        Thread.sleep(2000);
         System.out.println("c---123");
     }
     
+    
+    
+    
     public static void main(String[] args) throws Exception {
         ClassBlockingTest cb = new ClassBlockingTest();
-        cb.b();
-        ClassBlockingTest.a();
-        cb.c();
+        Thread t1 = new Thread(new TestThread1(cb));
+        Thread t2 = new Thread(new TestThread2(cb));
+        Thread t3 = new Thread(new TestThread3());
+        
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+    
+}
+
+class TestThread1 implements Runnable{
+    private ClassBlockingTest cbt;
+    public TestThread1(ClassBlockingTest cbt){
+        this.cbt = cbt;
+    }
+
+    @Override
+    public void run() {
+        try {
+            cbt.b();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class TestThread2 implements Runnable{
+    private ClassBlockingTest cbt;
+    public TestThread2(ClassBlockingTest cbt){
+        this.cbt = cbt;
+    }
+
+    @Override
+    public void run() {
+        try {
+            cbt.c();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class TestThread3 implements Runnable{
+    @Override
+    public void run() {
+        try {
+            ClassBlockingTest.a();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
