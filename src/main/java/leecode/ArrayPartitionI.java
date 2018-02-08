1da52b3d246cc8e54c4394d9038b15cf10670e0d
wangxiaoman <xiaoman@linkedme.cc>
2018-02-08 11:50:52 +0800

    【修改】代码调整

--- src/main/java/leecode/ArrayPartitionI.java
+++ src/main/java/leecode/ArrayPartitionI.java
@@ -3,16 +3,28 @@
  */
 package leecode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.util.CollectionUtils;
+
 /**
  * @author linkedme 561
  *
  */
 public class ArrayPartitionI {
     public static int arrayPairSum(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(Integer num : nums){
+            list.add(num);
+        }
+        Collections.sort(list);
+        
         int result = 0;
-        for (int i = 0; i < nums.length / 2; i++) {
-            int temp = nums[i] < nums[nums.length - i - 1] ? nums[i] : nums[nums.length - i - 1];
-            result = result + temp;
+        for (int i = 0; i < list.size() ; i = i+2) {
+            result = result + list.get(i);
         }
         return result;
     }
