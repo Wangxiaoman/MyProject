49adae073387b3b8d1f36c5ef82eeac13195aab0
wangxiaoman <xiaoman@linkedme.cc>
2018-02-11 10:49:44 +0800

    【修改】代码调整

---
+++ src/main/java/leecode/PartitionLabels.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package leecode;
+
+import java.util.List;
+
+/**
+ * @author linkedme
+ *  763
+ */
+public class PartitionLabels {
+    public static List<Integer> partitionLabels(String S) {
+        return null;
+    }   
+
+    public static void main(String[] args) {
+
+    }
+}
